2cd1d1fd74a9c5d511742e502e77bbd8e07060f8
Ochiogu Chisom Kingsley <iamcheesom@gmail.com>
2025-07-19 08:12:30 +0100

    Add files via upload

---
+++ graduation.js
@@ -0,0 +1,504 @@
+let Basic5 = []
+Basic5[0] = {
+    title: "ADIGWE MIRACLE",
+    fullname: "ADIGWE MIRACLE CHINEDU",
+    Class: "Basic 5",
+    image: "pictures/BASIC 5/ADIGWE MIRACLE CHINEDU.jpg",
+    Description: "Two young hearts exploring the world"
+}
+Basic5[1] = {
+    title: "AKPAN GOD'SWILL",
+    fullname: "AKPAN GOD'SWILL ASUABIAT",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/AKPAN GOD'SWILL ASUABIAT.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[2] = {
+    title: "ASHOGBON DANIEL",
+    fullname: "ASHOGBON DANIEL OLUWATOBI CHUKWUEBUKA",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/ASHOGBON DANIEL OLUWATOBI CHUKWUEBUKA.jpg",
+    Description: "A movie about a baby sitter"
+}
+
+Basic5[3] = {
+    title: "ASHOGBON DAVID",
+    fullname: "ASHOGBON DAVID TOLUWANI CHINEMERI",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/ASHOGBON DAVID TOLUWANI CHINEMERI.jpg",
+    Description: "A movie about a baby sitter"
+}
+
+Basic5[4] = {
+    title: "ASHOGBON DENNIS",
+    fullname: "ASHOGBON DENNIS OLUWATONI AKACHUKWU",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/ASHOGBON DENNIS OLUWATONI AKACHUKWU.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[5] = {
+     title: "AZUBUIKE MIRACLE",
+     fullname: "AZUBUIKE MIRACLE CHIDERA",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/AZUBUIKE MIRACLE CHIDERA.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[6] = {
+    title: "CHUKWUTEM REX",
+    fullname: "CHUKWUTEM REX CHUKWUSOM",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/CHUKWUTEM REX CHUKWUSOM.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[7] = {
+    title: "DAVID LAURETTE",
+    fullname: "DAVID LAURETTE IRENOSEN",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/DAVID LAURETTE IRENOSEN.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[8] = {
+    title: "EBINI BOMA",
+    fullname: "EBINI BOMA PHOEBE",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/EBINI BOMA PHOEBE.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[9] = {
+    title: "GEORGE ARIELA",
+    fullname: "GEORGE ARIELA JACKSON",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/GEORGE ARIELA JACKSON.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[10] = {
+    title: "ISRAEL GREAT",
+    fullname: "ISRAEL GREAT UDOCHUKWU",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/ISRAEL GREAT UDOCHUKWU.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[11] = {
+    title: "JACOBS DEBORAH",
+    fullname: "JACOBS DEBORAH AMARACHI",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/JACOBS DEBORAH AMARACHI .P..jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[12] = {
+    title: "JOHN EMMANUEL",
+    fullname: "JOHN EMMANUEL OBIESIA EDWARD",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/JOHN EMMANUEL OBIESIA EDWARD.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[13] = {
+    title: "MACLEAN PRECIOUS",
+    fullname: "MACLEAN PRECIOUS APETOU",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/MACLEAN PRECIOUS APETOU.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[14] = {
+    title: "OKORIE BELIEVE",
+    fullname: "OKORIE BELIEVE AKAN",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/OKORIE BELIEVE AKAN.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[15] = {
+    title: "OLAYANJU ELSA",
+    fullname: "OLAYANJU ELSA ANJOLAOLUWA",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/OLAYANJU ELSA ANJOLAOLUWA.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[16] = {
+    title: "OROBOGHA EWOMAZINA",
+    fullname: "OROBOGHA EWOMAZINA OLIVIA",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/OROBOGHA EWOMAZINA OLIVIA.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[17] = {
+    title: "REVIVAL SYLVESTER",
+    fullname: "REVIVAL SYLVESTER EXCEL UMOH",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/REVIVAL SYLVESTER EXCEL UMOH.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[18] = {
+    title: "ROBERT AZIBAODUSI",
+    fullname: "ROBERT AZIBAODUSI EZRA",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/ROBERT AZIBAODUSI EZRA.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[19] = {
+    title: "UZOMA DAVID",
+    fullname: "UZOMA DAVID CHIJINDU",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/UZOMA DAVID CHIJINDU.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[20] = {
+    title: "WELLINGTON DARASIMI",
+    fullname: "WELLINGTON DARASIMI STEPHANIE",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/WELLINGTON DARASIMI STEPHANIE.jpg",
+    Description: "A movie about a baby sitter"
+}
+Basic5[21] = {
+    title: "WOKE GABRIEL",
+    fullname: "WOKE GABRIEL OKWUCHUKWU",
+    Class: "Basic 5", 
+    image: "pictures/BASIC 5/WOKE GABRIEL OKWUCHUKWU.jpg",
+    Description: "A movie about a baby sitter"
+}
+
+let Js3 = []
+Js3[0] = {
+    title: "CHIGOZIE GOD'SWILL",
+    fullname: "CHIGOZIE GOD'SWILL CHUKWUGOZIE",
+    Class: "Jss 3",
+    image: "pictures/JSS3/CHIGOZIE GOD'SWILL CHUKWUGOZIE.jpg",
+    Description: "Two young hearts exploring the world"
+}
+Js3[1] = {
+    title: "CHUKWU CHIZZY",
+    fullname: "CHUKWU CHIZZY RUEBEN",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/CHUKWU CHIZZY RUEBEN.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[2] = {
+    title: "CYRIL AKUCHIE",
+    fullname: "CYRIL AKUCHIE WINNER CHIGOZIE",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/CYRIL AKUCHIE WINNER CHIGOZIE.jpg",
+    Description: "A movie about a baby sitter"
+}
+
+Js3[3] = {
+    title: "DAVIS HEPHZIBAH",
+    fullname: "DAVIS HEPHZIBAH AYANATE",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/DAVIS HEPHZIBAH AYANATE.jpg",
+    Description: "A movie about a baby sitter"
+}
+
+Js3[4] = {
+    title: "EMEH DELIGHT",
+    fullname: "EMEH DELIGHT CHIMEREMEZE",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/EMEH DELIGHT CHIMEREMEZE.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[5] = {
+     title: "EMEH DIVINE",
+     fullname: "EMEH DIVINE CHIKANYIMA",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/EMEH DIVINE CHIKANYIMA.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[6] = {
+    title: "GEORGE-JACKSON VANIA",
+    fullname: "GEORGE-JACKSON VANIA BOMA",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/GEORGE-JACKSON VANIA BOMA.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[7] = {
+    title: "HENRY FAVOUR",
+    fullname: "HENRY FAVOUR ODALONU",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/HENRY FAVOUR ODALONU.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[8] = {
+    title: "JOMBO HANNAH",
+    fullname: "JOMBO HANNAH MARCUS",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/JOMBO HANNAH MARCUS.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[9] = {
+    title: "LAZARUS FAVOUR",
+    fullname: "LAZARUS FAVOUR",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/LAZARUS FAVOUR.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[10] = {
+    title: "MACLEAN NKEREOWAJI",
+    fullname: "MACLEAN NKEREOWAJI IMAOWAJI",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/MACLEAN NKEREOWAJI IMAOWAJI.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[11] = {
+    title: "MARTINS-CROMWELL DELIGHT",
+    fullname: "MARTINS-CROMWELL DELIGHT OZIECHE",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/MARTINS-CROMWELL DELIGHT OZIECHE.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[12] = {
+    title: "MICHAEL OGOCHUKWU",
+    fullname: "MICHAEL OGOCHUKWU VICTORIA",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/MICHAEL OGOCHUKWU VICTORIA.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[13] = {
+    title: "OBOGUN SUCCESS",
+    fullname: "OBOGUN SUCCESS OLUWASEUN",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/OBOGUN SUCCESS OLUWASEUN.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[15] = {
+    title: "OHIE TEHILA",
+    fullname: "OHIE TEHILA ONODIOMA",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/OHIE TEHILA ONODIOMA.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[16] = {
+    title: "OMOH STEPHANIE",
+    fullname: "OMOH STEPHANIE CHIMSIMDI",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/OMOH STEPHANIE CHIMSIMDI.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[17] = {
+    title: "ONYEAGOROM GODREIGNETH",
+    fullname: "ONYEAGOROM GODREIGNETH CHIDAALU",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/ONYEAGOROM GODREIGNETH CHIDAALU.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[18] = {
+    title: "REVIVAL SUCCESS",
+    fullname: "ROBERT AZIBAODUSI EZRA",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/REVIVAL SUCCESS SYLVESTER.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[19] = {
+    title: "SOLOMON ESTHER",
+    fullname: "SOLOMON ESTHER ORUGBANI",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/SOLOMON ESTHER ORUGBANI.jpg",
+    Description: "A movie about a baby sitter"
+}
+Js3[20] = {
+    title: "WELLINGTON JEDIDIAH",
+    fullname: "WELLINGTON JEDIDIAH OLAOLUWA",
+    Class: "Jss 3", 
+    image: "pictures/JSS3/WELLINGTON JEDIDIAH OLAOLUWA.jpg",
+    Description: "A movie about a baby sitter"
+}
+
+function displayBasic5(filteredKids = Basic5) {
+  const container = document.getElementById("basic5-container");
+  const noResults = document.getElementById("no-results");
+  container.innerHTML = "";
+
+  const nameBox = document.getElementById("student-name");
+  const classBox = document.getElementById("student-class");
+  const quoteBox = document.getElementById("student-quote");
+  const studentBox = document.getElementById('student-box');
+  const cancel = document.getElementById("close");
+
+  noResults.style.display = 'none';
+  if (filteredKids.length === 0) {
+    noResults.style.display = "block";
+    return;
+  }
+
+  filteredKids.forEach(child => {
+    const card = document.createElement("div");
+    card.className = "class-card";
+
+    const img = document.createElement("img");
+    img.className = "class-image";
+    img.src = child.image;
+    img.alt = child.title;
+    img.addEventListener("click", () => {
+      nameBox.innerText = child.fullname;
+      classBox.innerText = "Class: " + child.Class;
+      quoteBox.innerText = "Favourite Quote: " + child.Description;
+      studentBox.style.display = "block";
+    });
+
+    card.innerHTML = `
+      <div class="movie-details">
+        <div id="kid-name">${child.title}</div>
+      </div>
+    `;
+    card.appendChild(img);
+    container.appendChild(card);
+  });
+
+  cancel.onclick = function() {
+    studentBox.style.display = 'none';
+  }
+
+  window.onclick = function(event) {
+    if (event.target == studentBox) {
+      studentBox.style.display = 'none';
+    }
+  }
+}
+
+displayBasic5()
+
+function displayJs3(filteredKids = Js3) {
+  const container = document.getElementById("js3-container");
+  const noResults = document.getElementById("no-results");
+  container.innerHTML = "";
+
+  const nameBox = document.getElementById("student-name");
+  const classBox = document.getElementById("student-class");
+  const quoteBox = document.getElementById("student-quote");
+  const studentBox = document.getElementById('student-box');
+  const cancel = document.getElementById("close");
+
+  noResults.style.display = 'none';
+  if (filteredKids.length === 0) {
+    noResults.style.display = "block";
+    return;
+  }
+
+  filteredKids.forEach(child => {
+    const card = document.createElement("div");
+    card.className = "class-card";
+
+    const img = document.createElement("img");
+    img.className = "class-image";
+    img.src = child.image;
+    img.alt = child.title;
+    img.addEventListener("click", () => {
+      nameBox.innerText = child.fullname;
+      classBox.innerText = "Class: " + child.Class;
+      quoteBox.innerText = "Favourite Quote: " + child.Description;
+      studentBox.style.display = "block";
+    });
+
+    card.innerHTML = `
+      <div class="movie-details">
+        <div id="kid-name">${child.title}</div>
+      </div>
+    `;
+    card.appendChild(img);
+    container.appendChild(card);
+  });
+
+  cancel.onclick = function() {
+    studentBox.style.display = 'none';
+  }
+
+  window.onclick = function(event) {
+    if (event.target == studentBox) {
+      studentBox.style.display = 'none';
+    }
+  }
+}
+
+displayJs3()
+
+function displaySs3(filteredKids = Ss3) {
+  const container = document.getElementById("ss3-container");
+  const noResults = document.getElementById("no-results");
+  container.innerHTML = "";
+
+  const nameBox = document.getElementById("student-name");
+  const classBox = document.getElementById("student-class");
+  const quoteBox = document.getElementById("student-quote");
+  const studentBox = document.getElementById('student-box');
+  const cancel = document.getElementById("close");
+
+  noResults.style.display = 'none';
+  if (filteredKids.length === 0) {
+    noResults.style.display = "block";
+    return;
+  }
+
+  filteredKids.forEach(child => {
+    const card = document.createElement("div");
+    card.className = "class-card";
+
+    const img = document.createElement("img");
+    img.className = "class-image";
+    img.src = child.image;
+    img.alt = child.title;
+    img.addEventListener("click", () => {
+      nameBox.innerText = child.fullname;
+      classBox.innerText = "Class: " + child.Class;
+      quoteBox.innerText = "Favourite Quote: " + child.Description;
+      studentBox.style.display = "block";
+    });
+
+    card.innerHTML = `
+      <div class="movie-details">
+        <div id="kid-name">${child.title}</div>
+      </div>
+    `;
+    card.appendChild(img);
+    container.appendChild(card);
+  });
+
+  cancel.onclick = function() {
+    studentBox.style.display = 'none';
+  }
+
+  window.onclick = function(event) {
+    if (event.target == studentBox) {
+      studentBox.style.display = 'none';
+    }
+  }
+}
+
+displaySs3()
+
+let slideIndex = 0;
+const slides = document.querySelectorAll(".group-slide");
+
+function showSlides() {
+  slides.forEach((slide, idx) => {
+    slide.classList.remove("active");
+  });
+  slideIndex++;
+  if (slideIndex > slides.length) slideIndex = 1;
+  slides[slideIndex - 1].classList.add("active");
+  setTimeout(showSlides, 4000);
+}
+
+window.addEventListener('DOMContentLoaded', showSlides);
+
+function searchStudents() {
+  const query = document.getElementById("searchInput").value.toLowerCase();
+  const noResults = document.getElementById("no-results");
+
+  // Filter each class
+  const filteredBasic5 = Basic5.filter(s => 
+    s.title.toLowerCase().includes(query) || s.fullname.toLowerCase().includes(query)
+  );
+  const filteredJs3 = Js3.filter(s => 
+    s.title.toLowerCase().includes(query) || s.fullname.toLowerCase().includes(query)
+  );
+  const filteredSs3 = Ss3.filter(s => 
+    s.title.toLowerCase().includes(query) || s.fullname.toLowerCase().includes(query)
+  );
+
+  // Display filtered results
+  displayBasic5(filteredBasic5);
+  displayJs3(filteredJs3);
+  displaySs3(filteredSs3);
+
+  const totalResults = filteredBasic5.length + filteredJs3.length + filteredSs3.length;
+  noResults.style.display = totalResults === 0 ? "block" : "none";
+}
+
+
